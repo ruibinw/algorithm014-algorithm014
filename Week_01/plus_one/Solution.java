e642cb99ebcb955a980c8dfa9b283413baba07d6
ruibin.w <ruibin.w@hotmail.com>
2020-08-11 16:49:25 +0800

    plus_one

---
+++ Week_01/plus_one/Solution.java
@@ -0,0 +1,13 @@
+package plus_one;
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--)
+            if ((digits[i] = ++digits[i] % 10) != 0)
+                return digits;
+        digits = new int[n + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
