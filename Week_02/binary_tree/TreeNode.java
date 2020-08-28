c86a9c27b515ee097b1682c348b50280357015c3
ruibin.w <ruibin.w@hotmail.com>
2020-08-28 13:47:08 +0800

    binary_tree TreeNode definition

---
+++ Week_02/binary_tree/TreeNode.java
@@ -0,0 +1,13 @@
+package binary_tree;
+
+/**
+ * Definition of TreeNode
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
