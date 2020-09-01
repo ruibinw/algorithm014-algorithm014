d76eff9b735369463602efa9c920b4c24a896181
ruibin.w <ruibin.w@hotmail.com>
2020-09-01 17:49:11 +0800

    groupAnagrams in py

---
+++ Week_02/group_anagrams/Solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        map = {}
+        for s in strs:
+            key = tuple(sorted(s))
+            map[key] = map.get(key, []) + [s]
+        return list(map.values())
+
+
+m = Solution().groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
+print(m)
