cf4abbbe377795a5a1bfb3f61a52d326f7fae04b
ruibin.w <ruibin.w@hotmail.com>
2020-09-17 14:01:49 +0800

    update subsets

--- Week_03/subsets/Solution.java
+++ Week_03/subsets/Solution.java
@@ -1,9 +1,10 @@
 package subsets;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-class Solution {
+class Solution1 {
 
     List<List<Integer>> res = new ArrayList<>();
 
@@ -20,9 +21,45 @@ class Solution {
             list.remove(list.size() - 1);
         }
     }
+}
 
+class Solution2 {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        dfs(nums, ans, 0, nums.length, new ArrayList<>());
+        return ans;
+    }
+    private void dfs(int[] nums, List<List<Integer>> ans, int i, int n, List<Integer> list) {
+        if (i == n) {
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        //选择放
+        list.add(nums[i]);
+        dfs(nums, ans, i + 1, n, list);
+        //选择不放
+        list.remove(list.size() - 1);
+        dfs(nums, ans, i + 1, n, list);
+    }
+}
+
+class Solution3 {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(Collections.EMPTY_LIST);
+        for (int num : nums) {
+            List<List<Integer>> temp = new ArrayList<>();
+            for (List<Integer> list : res) {
+                ArrayList<Integer> newList = new ArrayList<>(list);
+                newList.add(num);
+                temp.add(newList);
+            }
+            res.addAll(temp);
+        }
+        return res;
+    }
     public static void main(String[] args) {
-        List<List<Integer>> subsets = new Solution().subsets(new int[]{1, 2, 3});
+        List<List<Integer>> subsets = new Solution3().subsets(new int[]{1, 2, 3});
         System.out.println(subsets);
     }
 }
--- Week_03/subsets/Solution.py
+++ Week_03/subsets/Solution.py
@@ -1,3 +1,5 @@
+from typing import List
+
 # 递归回溯
 # Time: O(2^n), 每个元素有两种选择，放或者不放
 # Space: O(2^n)
@@ -5,15 +7,20 @@ class Solution1:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         res = []
-        def backtracking(i=0, list=[]):
+
+        def backtracking(i=0, path=[]):
             if i == n:
-                res.append(list)
+                res.append(path[:]) # 拷贝保存
                 return
-            backtracking(i + 1, list) # 不放元素
-            backtracking(i + 1, list + [nums[i]]) # 放元素
+            path.append(nums[i])
+            backtracking(i + 1, path)  # 放元素
+            path.pop()
+            backtracking(i + 1, path)  # 不放元素
 
         backtracking()
         return res
+
+print(Solution1().subsets([1,2,3]))
 
 # 循环，从第一个空子集开始，每次向子集添加新的整数
 class Solution2:
