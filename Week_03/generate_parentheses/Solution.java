80164295d81eed2f91ea385936368ca272c9c3e5
ruibin.w <ruibin.w@hotmail.com>
2020-09-23 14:32:19 +0800

    generateParenthesis

--- Week_03/generate_parentheses/Solution.java
+++ Week_03/generate_parentheses/Solution.java
@@ -36,28 +36,48 @@ class Test {
  * - 左括号个数不能大于 n
  * - 先有了左括号后才能加右括号 right < left
  */
-class Solution {
+class Solution1 {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
         generate(result, "", 0, 0, n);
         return result;
     }
 
-    private void generate(List<String> result, String s, int left, int right, int max) {
-        if (left == max && right == max) {
+    private void generate(List<String> result, String s, int L, int R, int n) {
+        if (L == n && R == n) {
             result.add(s);
             return;
         }
-        if (left < max) {
-            generate(result, s + "(", left + 1, right, max);
-        }
-        if (right < left) {
-            generate(result, s + ")", left, right + 1, max);
-        }
+        if (L < n) generate(result, s + "(", L + 1, R, n);
+        if (R < L) generate(result, s + ")", L, R + 1, n);
     }
 
     public static void main(String[] args) {
-        List<String> list = new Solution().generateParenthesis(2);
+        List<String> list = new Solution1().generateParenthesis(2);
         System.out.println(list);
+    }
+}
+
+/**
+ * 使用 StringBuilder，优化空间性能
+ */
+class Solution2 {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        generateParenthesis(n, ans, new StringBuilder(), 0, 0);
+        return ans;
+    }
+    private void generateParenthesis(int n, List<String> ans, StringBuilder s, int L, int R) {
+        if (L == n && R == n) {
+            ans.add(s.toString());
+        }
+        if (L < n) {
+            generateParenthesis(n, ans, s.append("("), L + 1, R);
+            s.deleteCharAt(s.length() - 1);
+        }
+        if (R < L) {
+            generateParenthesis(n, ans, s.append(")"), L, R + 1);
+            s.deleteCharAt(s.length() - 1);
+        }
     }
 }
