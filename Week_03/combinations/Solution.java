8dcf759988bf15ceec96c4f1181f26163bcb4124
ruibin.w <ruibin.w@hotmail.com>
2020-09-24 15:00:51 +0800

    combinations

--- Week_03/combinations/Solution.java
+++ Week_03/combinations/Solution.java
@@ -3,31 +3,53 @@ package combinations;
 import java.util.ArrayList;
 import java.util.List;
 
-class Solution {
-
-    List<List<Integer>> res = new ArrayList<>();
-
+/**
+ * 回溯
+ * Time: O(2^n)，每个数字可放可不放 2种选择
+ * Space: O(n) 递归深度，O(n!/((n-k)!k!))
+ */
+class Solution1 {
     public List<List<Integer>> combine(int n, int k) {
-        findCombination(n, k, new ArrayList<>(), 1);
-        return res;
+        List<List<Integer>> ans = new ArrayList<>();
+        dfs(ans, n, k, new ArrayList<>(), 1);
+        return ans;
     }
-
-    private void findCombination(int n, int k, List<Integer> list, int start) {
-        System.out.println(list);
-        if (list.size() == k) {
-            res.add(new ArrayList<>(list));
+    private void dfs(List<List<Integer>> ans, int n, int k, List<Integer> list, int i) {
+        if (i > n) {
             return;
         }
-        for (int i = start; i <= n - (k - list.size()) + 1; i++) { //剪枝：把 i <= n 改成 i <= n - (k - list.size()) + 1
-            list.add(i);
-            findCombination(n, k, new ArrayList<>(list), i + 1);
-            list.remove(list.size() - 1);
+        if (list.size() == k) {
+            ans.add(new ArrayList(list));
+            return;
         }
+        list.add(i);
+        dfs(ans, n, k, list, i + 1);
+        list.remove(list.size() - 1);
+        dfs(ans, n, k, list, i + 1);
     }
+}
 
-    public static void main(String[] args) {
-        List<List<Integer>> list = new Solution().combine(10, 5);
-        System.out.println(list);
-        System.out.println(list.size());
+/**
+ * 回溯 + 剪枝
+ */
+class Solution2 {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        dfs(ans, n, k, new ArrayList<>(), 1);
+        return ans;
+    }
+    private void dfs(List<List<Integer>> ans, int n, int k, List<Integer> list, int i) {
+        //剪枝：list长度加上区间 [i, n] 的长度小于 k，不可能构造出长度为 k 的组合
+        if (list.size() + (n - i + 1) < k) {
+            return;
+        }
+        if (list.size() == k) {
+            ans.add(new ArrayList(list));
+            return;
+        }
+        list.add(i);
+        dfs(ans, n, k, list, i + 1);
+        list.remove(list.size() - 1);
+        dfs(ans, n, k, list, i + 1);
     }
 }
