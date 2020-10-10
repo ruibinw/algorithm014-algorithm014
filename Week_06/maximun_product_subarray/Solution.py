330182e17200e55aa793991d7a6207659b91a400
ruibin.w <ruibin.w@hotmail.com>
2020-10-10 14:16:57 +0800

    max_product_subarray

--- Week_06/maximun_product_subarray/Solution.py
+++ Week_06/maximun_product_subarray/Solution.py
@@ -1,11 +1,10 @@
 # 维护两个变量，当前最小乘积 cur_min，和当前最大乘积 cur_max
-class Solution1:
+class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        cur_max, cur_min, max_so_far = nums[0], nums[0], nums[0]
-        for i in range(1, len(nums)):
-            if nums[i] < 0:
-                cur_max, cur_min = cur_min, cur_max
-            cur_max = max(cur_max * nums[i], nums[i])
-            cur_min = min(cur_min * nums[i], nums[i])
-            max_so_far = max(max_so_far, cur_max)
-        return max_so_far
+        cur_max = cur_min = max_prd = nums[0]
+        for n in nums[1:]:
+            if n < 0: cur_max, cur_min = cur_min, cur_max
+            cur_max = max(cur_max * n, n)
+            cur_min = min(cur_min * n, n)
+            max_prd = max(max_prd, cur_max)
+        return max_prd
