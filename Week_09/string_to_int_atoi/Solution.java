ba9054d3c04f38a0d5b4db2c4ac6dc41e1b89586
ruibin.w <ruibin.w@hotmail.com>
2020-10-20 09:50:31 +0800

    string_to_int_atoi

--- Week_09/string_to_int_atoi/Solution.java
+++ Week_09/string_to_int_atoi/Solution.java
@@ -3,23 +3,23 @@ package string_to_int_atoi;
 class Solution {
     public int myAtoi(String s) {
         char[] c = s.toCharArray();
-        int i = 0, total = 0, sign = 1;
+        int i = 0;
+        long total = 0;
+        boolean isPlus = true;
         //remove space
         while (i < c.length && c[i] == ' ') i++;
         //empty string
         if (i >= c.length) return 0;
         //handle sign
-        if (c[i] == '-' || c[i] == '+') {
-            sign = s.charAt(i++) == '-' ? -1 : 1;
-        }
+        if (c[i] == '-' || c[i] == '+')
+            isPlus = c[i++] == '+' ? true : false;
         //convert number
         while (i < c.length && c[i] >= '0' && c[i] <= '9') {
-            int digit = s.charAt(i++) - '0';
-            //Integer.MAX_VALUE = 2147483647, Integer.MAX_VALUE/10 = 214748364, Integer.MAX_VALUE%10 = 7,
-            if (total > 214748364 || (total == 214748364 && digit > 7))
-                return sign == -1 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            int digit = c[i++] - '0';
             total = total * 10 + digit;
+            if (total > Integer.MAX_VALUE)
+                return isPlus ? Integer.MAX_VALUE : Integer.MIN_VALUE;
         }
-        return total * sign;
+        return (int)(isPlus ? total : -total);
     }
 }
