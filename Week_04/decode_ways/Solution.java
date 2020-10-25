76c9a18b3233ad75c2c969f737faa50e89ca2a2a
ruibin.wang <ruibin.w@hotmail.com>
2020-10-25 18:24:33 +0800

    decode_ways

--- Week_04/decode_ways/Solution.java
+++ Week_04/decode_ways/Solution.java
@@ -8,7 +8,7 @@ import java.util.Map;
  * Time: O(n)，加了缓存不用重复计算
  * Space：O(n)，递归最深有 n 层
  */
-class Solution {
+class Solution1 {
     public int numDecodings(String s) {
         return s.length() == 0 ? 0 : numDecodings(s, 0, new HashMap<>());
     }
@@ -24,5 +24,59 @@ class Solution {
 
         memo.put(i, count);
         return count;
+    }
+}
+
+/**
+ * dp，O(n), O(n)
+ */
+class Solution2 {
+    public int numDecodings(String s) {
+        int n = s.length();
+        if (n == 0) return 0;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
+        for (int i = 2; i <= n; i++) {
+            int one = Integer.valueOf(s.substring(i - 1, i));
+            if (one >= 1 && one <= 9) {
+                dp[i] += dp[i - 1];
+            }
+            int two = Integer.valueOf(s.substring(i - 2, i));
+            if (two >= 10 && two <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
+}
+
+/**
+ * dp，O(n), O(1)
+ */
+class Solution3 {
+    public int numDecodings(String s) {
+        int n = s.length();
+        if (n == 0) return 0;
+
+        int pre = 1, cur = s.charAt(0) == '0' ? 0 : 1;
+
+        for (int i = 2; i <= n; i++) {
+            int next = 0;
+
+            int one = Integer.valueOf(s.substring(i - 1, i));
+            if (one >= 1 && one <= 9) {
+                next += cur;
+            }
+            int two = Integer.valueOf(s.substring(i - 2, i));
+            if (two >= 10 && two <= 26) {
+                next += pre;
+            }
+            pre = cur;
+            cur = next;
+        }
+        return cur;
     }
 }
