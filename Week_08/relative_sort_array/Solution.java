75cced1968a43afa640f090e74549e186cf2cb39
ruibin.w <ruibin.w@hotmail.com>
2020-10-29 14:44:56 +0800

    relativeSortArray

--- Week_08/relative_sort_array/Solution.java
+++ Week_08/relative_sort_array/Solution.java
@@ -11,22 +11,17 @@ import java.util.TreeMap;
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
         Map<Integer, Integer> bst = new TreeMap<>();//Binary Search Tree
-        for (int num : arr1) {
-            bst.put(num, bst.getOrDefault(num, 0) + 1);
-        }
+        for (int num : arr1) bst.put(num, bst.getOrDefault(num, 0) + 1);
 
         int i = 0;
         for (int num : arr2) {
-            int count = bst.getOrDefault(num, 0);
-            while (count-- > 0) {
+            for (int j = 0; j < bst.get(num); j++) {
                 arr1[i++] = num;
             }
             bst.remove(num);
         }
-
         for (int num : bst.keySet()) {
-            int count = bst.get(num);
-            while (count-- > 0) {
+            for (int j = 0; j < bst.get(num); j++) {
                 arr1[i++] = num;
             }
         }
